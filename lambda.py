d782638cdfd2d22acc4dc59785c3faa03d6f82bb
Nurudeen Adesina <45924101+A-Nuru@users.noreply.github.com>
2021-09-16 23:34:04 +0100

    get access to aws resources

--- lambda.py
+++ lambda.py
@@ -4,3 +4,14 @@ import json
 #third party library in python to access aws
 import boto3
 import datetime
+# define a lambda_handler function
+def lambda_handler(event, context):
+    
+    
+    #resource accessed from boto library
+    dynamo_db = boto3.resource('dynamodb')
+    
+    ##access created dynamoDB table name "MyTable"
+    table = dynamo_db.Table('MyTable')
+        
+   
