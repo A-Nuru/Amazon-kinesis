e8793bb5e12d02a04412a5a30efec99e8e9f0504
Nurudeen Adesina <45924101+A-Nuru@users.noreply.github.com>
2021-09-29 10:53:14 +0100

    writing data into MyTable DynamoDB

--- lambda.py
+++ lambda.py
@@ -8,10 +8,22 @@ import datetime
 def lambda_handler(event, context):
     
     
-    #resource accessed from boto library
-    dynamo_db = boto3.resource('dynamodb')
+    try:
+        #resource accessed from boto library
+        dynamo_db = boto3.resource('dynamodb')
     
-    ##access created dynamoDB table name "MyTable"
-    table = dynamo_db.Table('MyTable')
+        ##access created dynamoDB table name "MyTable"
+        table = dynamo_db.Table('MyTable')
+    
+    
+        decoded_data =  [base64.b64decode(record["kinesis"]["data"]).decode("utf-8") for record in event["Records"]]
+        print(decoded_data)
+        print(json.loads(decoded_data))
+        decoded_data_dic = json.loads(decoded_data)
+        with table.batch_writer() as batch_writer:
+            batch_writer.put_item(Item=decoded_data_dic)
+
+    except Exception as e: 
+        print(str(e))
         
    
